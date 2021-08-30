5afcb74f1b262cc8fa39132881178a186993f5f5
SalmaKhaled95 <30567586+SalmaKhaled95@users.noreply.github.com>
2021-08-30 17:34:43 +0200

    Add files via upload

---
+++ Task4/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function LogOutButton() {
+
+    console.log("LogOut!");
+}
+
+function JoinGroupButton() {
+    console.log("JoinGroup!");
+}
+
+
+function SendMessageButton() {
+    console.log("Send Message!");
+}
+
+//document.getElementById("join").addEventListener = JoinGroupButton();
