665d9c8f2c71a7bea8ff7000aa1c5356963a62a2
SalmaKhaled95 <30567586+SalmaKhaled95@users.noreply.github.com>
2021-09-17 04:00:48 +0200

    Add files via upload
    PHP Task1, Sunday 12 Sep 2021

---
+++ index.php
@@ -0,0 +1,461 @@
+ <?php
+			  
+			  
+			  $_REQUEST['username']= "Admin";
+			  $_REQUEST['password'] = "1234567890";
+			  if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+				  
+				  $conn = mysqli_connect('localhost', 'root', '', 'blog');
+				  if ($conn)
+				  {
+					  $username = $_REQUEST['username'];
+					  $password = $_REQUEST['password'];
+					  $SQL = "SELECT * FROM `posts` ORDER BY `publish_date` Desc  LIMIT 4;";
+					  $query = mysqli_query( $conn , $SQL );
+					  //if ($conn->connect_error) {echo "error";}
+					  //else {echo "ok";}
+					  //while($row = mysqli_fetch_assoc($query)) {
+					  //echo print_r($row);       // Print the entire row data
+					  //echo "\n";}
+					  
+					  
+				  }
+				  
+			  }
+			  
+			  
+			  ?>
+<!DOCTYPE HTML>
+    <html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+
+    <title>Stand CSS Blog by TemplateMo</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-stand-blog.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+
+TemplateMo 551 Stand Blog
+
+https://templatemo.com/tm-551-stand-blog
+
+-->
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.php"><h2>Sprints Blog<em>.</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="posts.php">Posts</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="logout.php">Logout</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="register.php">Register</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="admin.php">Admin</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="myposts.php">My Posts</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text">
+      <div class="container-fluid">
+        <div class="owl-banner owl-carousel">
+          <div class="item">
+            <img src="assets/images/banner-item-01.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Fashion</span>
+                </div>
+                <a href="post-details.html"><h4>Morbi dapibus condimentum</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 12, 2020</a></li>
+                  <li><a href="#">12 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/banner-item-02.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Nature</span>
+                </div>
+                <a href="post-details.html"><h4>Donec porttitor augue at velit</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 14, 2020</a></li>
+                  <li><a href="#">24 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/banner-item-03.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Lifestyle</span>
+                </div>
+                <a href="post-details.html"><h4>Best HTML Templates on TemplateMo</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 16, 2020</a></li>
+                  <li><a href="#">36 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/banner-item-04.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Fashion</span>
+                </div>
+                <a href="post-details.html"><h4>Responsive and Mobile Ready Layouts</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 18, 2020</a></li>
+                  <li><a href="#">48 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/banner-item-05.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Nature</span>
+                </div>
+                <a href="post-details.html"><h4>Cras congue sed augue id ullamcorper</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 24, 2020</a></li>
+                  <li><a href="#">64 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/banner-item-06.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Lifestyle</span>
+                </div>
+                <a href="post-details.html"><h4>Suspendisse nec aliquet ligula</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 26, 2020</a></li>
+                  <li><a href="#">72 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <section class="blog-posts">
+      <div class="container">
+        <div class="row">
+		
+		
+          <div class="col-lg-8">
+            <div class="all-blog-posts">
+              <div class="row">
+			  
+			 <?php
+			 
+			 $_REQUEST['username']= "Admin";
+			  $_REQUEST['password'] = "1234567890";
+			  if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+				  
+				  $conn = mysqli_connect('localhost', 'root', '', 'blog');
+				  if ($conn)
+				  {
+					  $username = $_REQUEST['username'];
+					  $password = $_REQUEST['password'];
+					  $SQL = "SELECT * FROM `posts` ORDER BY `publish_date` Desc  LIMIT 4;";
+					  $query = mysqli_query( $conn , $SQL );
+					  
+					  
+					  
+					  
+					  //if ($conn->connect_error) {echo "error";}
+					  //else {echo "ok";}
+					  //id, title, content, publish_date, created_at, updated_at, category_id, user_id, image
+			 while ($row = mysqli_fetch_assoc($query) ){
+				
+				// echo print_r($row);       // Print the entire row data
+				 $TheID = $row['category_id'];
+				 $userID = $row['user_id'];
+					//  echo "\n";
+					  
+					  $categoriesSQL = "SELECT * FROM `categories` WHERE `id` = " .$TheID .";";
+					  $categoriesQuery = mysqli_query( $conn , $categoriesSQL );
+					  $categoriesRow = mysqli_fetch_assoc($categoriesQuery);
+					  
+					  $UserSQL = "SELECT * FROM `users` WHERE `id` = " .$userID .";";
+					  $UserQuery = mysqli_query( $conn , $UserSQL );
+					  $UserRow = mysqli_fetch_assoc($UserQuery);
+					  
+					  $CommentSQL = "SELECT IFNULL((SELECT COUNT(*) as CountColumn FROM `comments` WHERE `post_id` = " .$row['id'] ."),0) As countcl";  
+					  $CommentQuery = mysqli_query( $conn , $CommentSQL );
+					  $CommentRow = mysqli_fetch_assoc($CommentQuery);
+					  
+					  
+					  $TagsSQL = "SELECT `tag_id` FROM `post_tags` WHERE `post_id` = " .$row['id'] .";";  
+					  $TagsQuery = mysqli_query( $conn , $TagsSQL );
+					 
+
+
+					  
+					 // echo "this one /n";
+					  //echo $categoriesRow; 
+			 ?>
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src= <?php echo  $row['image']   ?> alt="">
+                    </div>
+                    <div class="down-content">
+                      <span><?php echo $categoriesRow['name'] ?></span>
+                      <a href="post-details.html"><h4><?php echo  $row['title']   ?> </h4></a>
+                      <ul class="post-info">
+                        <li><a href="#"><?php echo  $UserRow['name']   ?> </a></li>
+                        <li><a href="#"><?php echo  $row['publish_date']   ?></a></li>
+                        <li><a href="#"><?php echo  $CommentRow['countcl']   ?> Comments</a></li>
+                      </ul>
+                      <p><?php echo  $row['content']   ?> </p>
+					  <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+							   <?php
+							   
+							  while( $TagsRow = mysqli_fetch_assoc($TagsQuery)){
+								  foreach ($TagsRow as $OneTagID){
+								  $TagNameSQL = "SELECT `name` FROM `tags` WHERE `id` = " .$OneTagID  .";"; 
+//echo		$TagNameSQL;		  
+					  $TagNameQuery = mysqli_query( $conn , $TagNameSQL );
+					 $TagNameRow = mysqli_fetch_assoc($TagNameQuery);
+					 // echo  $TagNameRow;
+								  
+								  //
+								  
+					    ?>
+                              <li><a href="#"> <?php  echo  $TagNameRow['name']  ?></a>,</li>
+                              <?php
+							  }}
+							    ?>
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+							 
+                              <li><a href="#">Facebook</a>,</li>
+							<li><a href="#">Twitter</a></li>
+                             
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+             <?php
+			  }}}
+			 ?>
+				
+				
+                  
+                <div class="col-lg-12">
+                  <div class="main-button">
+                    <a href="blog.html">View All Posts</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="sidebar">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="sidebar-item search">
+                    <form id="search_form" name="gs" method="GET" action="#">
+                      <input type="text" name="q" class="searchText" placeholder="type to search..." autocomplete="on">
+                    </form>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="sidebar-item recent-posts">
+                    <div class="sidebar-heading">
+                      <h2>Recent Posts</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                        <li><a href="post-details.html">
+                          <h5>Vestibulum id turpis porttitor sapien facilisis scelerisque</h5>
+                          <span>May 31, 2020</span>
+                        </a></li>
+                        <li><a href="post-details.html">
+                          <h5>Suspendisse et metus nec libero ultrices varius eget in risus</h5>
+                          <span>May 28, 2020</span>
+                        </a></li>
+                        <li><a href="post-details.html">
+                          <h5>Swag hella echo park leggings, shaman cornhole ethical coloring</h5>
+                          <span>May 14, 2020</span>
+                        </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="sidebar-item categories">
+                    <div class="sidebar-heading">
+                      <h2>Categories</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                        <li><a href="#">- Nature Lifestyle</a></li>
+                        <li><a href="#">- Awesome Layouts</a></li>
+                        <li><a href="#">- Creative Ideas</a></li>
+                        <li><a href="#">- Responsive Templates</a></li>
+                        <li><a href="#">- HTML5 / CSS3 Templates</a></li>
+                        <li><a href="#">- Creative &amp; Unique</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="sidebar-item tags">
+                    <div class="sidebar-heading">
+                      <h2>Tag Clouds</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                        <li><a href="#">Lifestyle</a></li>
+                        <li><a href="#">Creative</a></li>
+                        <li><a href="#">HTML5</a></li>
+                        <li><a href="#">Inspiration</a></li>
+                        <li><a href="#">Motivation</a></li>
+                        <li><a href="#">PSD</a></li>
+                        <li><a href="#">Responsive</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="social-icons">
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Behance</a></li>
+              <li><a href="#">Linkedin</a></li>
+              <li><a href="#">Dribbble</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-12">
+            <div class="copyright-text">
+              <p>Copyright 2020 Sprints Blog Co.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
